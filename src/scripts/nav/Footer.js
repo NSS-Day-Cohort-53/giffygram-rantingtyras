6fc57a466dd3d0afd9ec1fc9c4137dc599d8c167
sorachung <sorachung.src@gmail.com>
2022-01-05 11:26:33 -0600

    shows select options for years and users

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,14 +1,22 @@
-import { getUsers } from "../data/provider.js";
+import { getUsers, getPosts } from "../data/provider.js";
 
 export const Footer = () => {
     const users = getUsers();
+    const posts = getPosts();
+    const yearsSet = new Set();
+    posts.forEach(post => {
+        const year = new Date(post.timestamp).getFullYear();
+        yearsSet.add(year);
+    })
+    const yearsArray = Array.from(yearsSet);
+    yearsArray.sort((a, b) => a - b);
 
     return `
         <footer class="footer">
             <div class="footer__item">
                 <select id="yearSelection">
                     <option value="0">Select year</option>
-                    
+                    ${yearsArray.map(year => `<option value=year>${year}</option>`).join("")}
                 </select>
                 <span id="postCount"></span>
             </div>
@@ -16,7 +24,7 @@ export const Footer = () => {
                 <label for="userSelection">Posts by user</label>
                 <select id="userSelection" name="userSelection">
                     <option value="0">Select a User</option>
-                    
+                    ${users.map(user => `<option value=${user.id}>${user.name}</option>`).join("")}
                 </select>
             </div>
             <div class="footer__item">
