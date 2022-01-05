02393d11789ac390b12ae7a65d1f01e13c8f7023
sorachung <sorachung.src@gmail.com>
2022-01-05 12:16:13 -0600

    added eventListener for selecting options in footer

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,4 +1,4 @@
-import { getUsers, getPosts } from "../data/provider.js";
+import { getUsers, getPosts, toggleFeedDisplayFavorites, setFeedChosenUser, getFeed } from "../data/provider.js";
 
 export const Footer = () => {
     const users = getUsers();
@@ -23,14 +23,32 @@ export const Footer = () => {
             <div class="footer__item">
                 <label for="userSelection">Posts by user</label>
                 <select id="userSelection" name="userSelection">
-                    <option value="0">Select a User</option>
-                    ${users.map(user => `<option value=${user.id}>${user.name}</option>`).join("")}
+                    <option value="0" ${getFeed().chosenUser === null ? `selected` : ''}>All</option></option>
+                    ${users.map(user => `<option value=${user.id} ${getFeed().chosenUser === user.id ? `selected` : ''}>${user.name}</option>`).join("")}
                 </select>
             </div>
             <div class="footer__item">
                 <label for="showOnlyFavorites">Show only favorites</label>
-                <input id="showOnlyFavorites" type="checkbox" />
+                <input id="showOnlyFavorites" type="checkbox" ${getFeed().displayFavorites ? "checked" : ""} />
             </div>
         </footer>
     `;
 };
+
+const applicationElement = document.querySelector(".giffygram");
+
+applicationElement.addEventListener("change", (event) => {
+    if (event.target.id === "showOnlyFavorites") {
+        toggleFeedDisplayFavorites();
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+    } else if (event.target.id === "userSelection") {
+        if (event.target.value === 0) {
+            setFeedChosenUser(null)
+        } else {
+            setFeedChosenUser(parseInt(event.target.value))
+        }
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+    } else if (event.target.id === "yearSelection") {  
+        applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+    }
+});
