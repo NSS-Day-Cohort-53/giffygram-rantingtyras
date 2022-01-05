a31e52a078582e0e2ff2d4828d1c8af660c37d16
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-01-05 16:19:15 -0500

    got delete render fixed

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,4 +1,4 @@
-import { getCurrentUser, getFeed, getLikes, getPosts, getUsers } from "../data/provider.js";
+/*import { getCurrentUser, getFeed, getLikes, getPosts, getUsers } from "../data/provider.js";
 
 export const postFeed = () => {
     let posts = getPosts();
@@ -39,4 +39,61 @@ export const postFeed = () => {
     html += buildPostFeed.join("");
     html += `</ul>`;
     return html;
-};
+};*/
+
+
+
+
+
+
+
+
+import { getPosts, getUsers, deletePost } from "../data/provider.js";
+import { renderApp } from "../main.js";
+
+export const postFeed = () => {
+    const posts = getPosts()
+    const users = getUsers()
+    let html = `<ul>`
+    const buildPostFeed = posts.map((post) => {
+        let foundUser = users.find(user => user.id === post.userId);
+
+        let currentUserId =localStorage.gg_user;
+     if (parseInt(currentUserId) === parseInt(foundUser.id))
+     {
+         return `<li>
+         <p>${post.title}</p>
+         <p><img src="${post.imageUrl}" alt=""></p>
+         <p>${post.description}</p>
+         <p>Post By: ${foundUser.name} at ${post.timestamp}</p>
+         <button id="deletePost--button-${post.id}">Delete</button>
+         </li>
+         <br></br>
+         <br></br>
+         <br></br>`
+     }
+     else 
+     {
+         return `<li>
+         <p>${post.title}</p>
+         <p><img src="${post.imageUrl}" alt=""></p>
+         <p>${post.description}</p>
+         <p>Post By: ${foundUser.name} at ${post.timestamp}</p>
+         </li>`
+     }
+     })
+     //this line will populate a delete button only if the gif belongs to the logged in user
+
+     html += buildPostFeed.join("")
+     html+=`</ul>`
+     return html
+    }
+
+    document.addEventListener("click", (event)=> {
+        if (event.target.id.startsWith("deletePost--button"))
+        {
+            let postId =event.target.id.substr(19);
+            deletePost(postId)
+            renderApp();
+        }
+    }) 
