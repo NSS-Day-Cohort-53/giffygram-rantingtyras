3c14ff1116e1dfe7953c7a9145315575e758875d
jbyrd4 <jbyrd4@gmail.com>
2022-01-05 13:37:47 -0600

    working on favorite button

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -11,9 +11,25 @@ export const postFeed = () => {
             <p><img src="${post.imageUrl}" alt=""></p>
             <p>${post.description}</p>
             <p>Post By: ${foundUser.name} at ${post.timestamp}</p>
+            <img class="star__notFave" id="${post.id}" name="star__notFave" src="../images/favorite-star-blank.svg" alt="">
             </li>`
         })
         html += buildPostFeed.join("")
         html+=`</ul>`
         return html
 }
+
+document.addEventListener("click", clickEvent => {
+    if (clickEvent.target.name === "star__notFave") {
+        const posts = getPosts()
+        favPost = posts.find(post => post.id === clickEvent.target.id)
+        const userId = localStorage.gg_user
+        const postId = favPost.id
+
+        const dataToSendToApi = {
+                userId: userId,
+                postId: postId
+        }
+        sendFav(dataToSendToApi)
+    }
+    })
