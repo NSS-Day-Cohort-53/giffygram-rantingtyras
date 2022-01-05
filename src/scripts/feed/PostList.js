faf5cf57655144dcba7300981c1405f4318a9cc7
sorachung <sorachung.src@gmail.com>
2022-01-05 13:36:35 -0600

    added filter by year and by user

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,19 +1,38 @@
-import { getPosts, getUsers } from "../data/provider.js";
+import { getFeed, getPosts, getUsers } from "../data/provider.js";
 
 export const postFeed = () => {
-    const posts = getPosts()
-    const users = getUsers()
-    let html = `<ul>`
+    let posts = getPosts();
+    let users = getUsers();
+    const feed = getFeed();
+
+    // change posts array to include only those of the chosen year
+    if (feed.chosenYear) {
+        posts = posts.filter(post => {
+            const year = new Date(post.timestamp).getFullYear();
+            return feed.chosenYear === year;
+        })
+    }
+
+    // change posts array to include only those of the chosen user
+    if (feed.chosenUser) {
+        posts = posts.filter(post => feed.chosenUser === post.userId)
+    }
+
+    if (feed.displayFavorites) {
+        
+    }
+
+    let html = `<ul>`;
     const buildPostFeed = posts.map((post) => {
-            let foundUser = users.find(user => user.id === post.userId)
-            return `<li>
+        let foundUser = users.find((user) => user.id === post.userId);
+        return `<li>
             <p>${post.title}</p>
             <p><img src="${post.imageUrl}" alt=""></p>
             <p>${post.description}</p>
             <p>Post By: ${foundUser.name} at ${post.timestamp}</p>
-            </li>`
-        })
-        html += buildPostFeed.join("")
-        html+=`</ul>`
-        return html
-}
+            </li>`;
+    });
+    html += buildPostFeed.join("");
+    html += `</ul>`;
+    return html;
+};
