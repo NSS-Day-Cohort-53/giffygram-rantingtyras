720cc25df0fe371ada612fe4e885d4caea911283
jbyrd4 <jbyrd4@gmail.com>
2022-01-05 15:07:04 -0600

    added ability to favorite and unfavorite

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,8 +1,11 @@
-import { getPosts, getUsers } from "../data/provider.js";
+import { getPosts, getUsers, sendFav, getLikes, deleteFav } from "../data/provider.js";
+
+let star
 
 export const postFeed = () => {
     const posts = getPosts()
     const users = getUsers()
+    const likes = getLikes()
     let html = `<ul>`
     const buildPostFeed = posts.map((post) => {
             let foundUser = users.find(user => user.id === post.userId)
@@ -11,25 +14,34 @@ export const postFeed = () => {
             <p><img src="${post.imageUrl}" alt=""></p>
             <p>${post.description}</p>
             <p>Post By: ${foundUser.name} at ${post.timestamp}</p>
-            <img class="star__notFave" id="${post.id}" name="star__notFave" src="../images/favorite-star-blank.svg" alt="">
+            <img id="${post.id}" ${likes.find(like => like.userId === parseInt(localStorage.gg_user) && like.postId === post.id) ? star = `class="star" name="star__fave" src="../images/favorite-star-yellow.svg" alt="Yellow Star"` : star = `class="star" name="star__notFave" src="../images/favorite-star-blank.svg" alt="Empty Star"`}>
             </li>`
         })
         html += buildPostFeed.join("")
-        html+=`</ul>`
+        html +=`</ul>`
         return html
 }
+
 
 document.addEventListener("click", clickEvent => {
     if (clickEvent.target.name === "star__notFave") {
         const posts = getPosts()
-        favPost = posts.find(post => post.id === clickEvent.target.id)
-        const userId = localStorage.gg_user
+        const favPost = posts.find(post => post.id === parseInt(clickEvent.target.id))
+        const userId = parseInt(localStorage.gg_user)
         const postId = favPost.id
 
         const dataToSendToApi = {
                 userId: userId,
                 postId: postId
         }
+
         sendFav(dataToSendToApi)
+    } else {
+        if (clickEvent.target.name === "star__fave") {
+            const likes = getLikes()
+            const unFavePost = likes.find((like) => like.postId === parseInt(clickEvent.target.id) && like.userId === parseInt(localStorage.gg_user))
+            const unFaveId = unFavePost.id
+            deleteFav(unFaveId)
+        }
     }
-    })
+})
