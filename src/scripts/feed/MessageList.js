2c44714c4845103752af64d11851f1fb6186a1f1
sorachung <sorachung.src@gmail.com>
2022-01-06 09:56:50 -0600

    displayMessages feed shows read and unread messages address to current user

--- src/scripts/feed/MessageList.js
+++ src/scripts/feed/MessageList.js
@@ -1,3 +1,20 @@
+import { getMessages, getUsers, getCurrentUser } from "../data/provider.js"
+
 export const MessageList = () => {
-    return `<h1>Messages</h1>`
+    let messages = getMessages();
+    const users = getUsers();
+    const currentUser = getCurrentUser();
+
+    messages = messages.filter(message => message.recipientId === currentUser.id);
+
+    return `
+        <article>
+            ${messages.map(message => {
+                return `
+                    <section class="message">
+                        <p class="message__author">${users.find(user => message.userId === user.id).name}</p>
+                        <p>${message.text}</p>
+                    </section>`
+            }).join("")}
+        </article>`
 }
