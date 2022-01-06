f0e52ae921db857ea1c8a016d74657702b9eb310
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-01-06 13:02:46 -0500

    fixed key names

--- src/scripts/friends/DirectMessage.js
+++ src/scripts/friends/DirectMessage.js
@@ -2,10 +2,10 @@ import { sendMsg } from "../data/provider.js"
 
 export const MessagePackager = (sender, recipient, message, readStatus) => {
     const messageObj = {
-        senderId: sender,
+        userId: sender,
         recipientId: recipient,
-        messageText: message,
-        readStatus: false
+        text: message,
+        read: false
     }
 
     sendMsg(messageObj);
