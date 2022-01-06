50d871a7afda58a5536d4aae0107d83ca602fb28
jbyrd4 <jbyrd4@gmail.com>
2022-01-06 14:14:51 -0600

    merging main

--- src/scripts/feed/Profile.js
+++ src/scripts/feed/Profile.js
@@ -10,17 +10,29 @@ export const profileFeed = () => {
     const user = users.find(user => user.id === userId)
     const userPosts = posts.filter(post => post.userId === user.id)
     const numOfPosts = userPosts.length
-    const messageArr = messages.filter((message) => {
-        (message.userId === currentUser.id && message.recipientId === user.id)
-        // ||
-        // (message.userId === user.id && message.recipientId === currentUser.id)
-    })
-    console.log(messageArr)
+    const messageSentArr = messages.filter(message => (message.userId === currentUser.id) && (message.recipientId === user.id))
+    const messageRecArr = messages.filter(message => (message.userId === user.id) && (message.recipientId === currentUser.id))
+    const messageArr = messageSentArr.concat(messageRecArr)
+    const messageList = () => {
+        let html = `<ul>`
+        const messageArray = messageArr.map((message) => {
+            return `<li>
+            <p>From: ${message.userId}</p>
+            <p>To: ${message.recipientId}</p>
+            <p>${message.text}</p>
+            </li>`
+        })
+
+        html += messageArray.join("")
+        html += `</ul>`
+        return html
+    }
+    
     return `
     <h2>${user.name}</h2>
     <br>
     <p>Total Number of Posts: ${numOfPosts}</p>
+    <p>${messageList()}</p>
     `
 }
 //Messages Between logged in and profiled user in descending chronological order
-
