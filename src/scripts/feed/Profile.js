b6cee1d95f686613e14da6a470e14c73d96fc506
jbyrd4 <jbyrd4@gmail.com>
2022-01-06 13:33:44 -0600

    merging new main

--- src/scripts/feed/Profile.js
+++ src/scripts/feed/Profile.js
@@ -1,18 +1,26 @@
-import { getUsers, getPosts, getCurrentUser, getFeed } from "../data/provider.js";
+import { getUsers, getPosts, getCurrentUser, getFeed, getMessages } from "../data/provider.js";
 
 export const profileFeed = () => {
     const feed = getFeed()
     const users = getUsers()
     const posts = getPosts()
+    const currentUser = getCurrentUser()
+    const messages = getMessages()
     const userId = parseInt(feed.UserProfileId)
     const user = users.find(user => user.id === userId)
     const userPosts = posts.filter(post => post.userId === user.id)
     const numOfPosts = userPosts.length
+    const messageArr = messages.filter((message) => {
+        (message.userId === currentUser.id && message.recipientId === user.id)
+        // ||
+        // (message.userId === user.id && message.recipientId === currentUser.id)
+    })
+    console.log(messageArr)
     return `
     <h2>${user.name}</h2>
     <br>
     <p>Total Number of Posts: ${numOfPosts}</p>
     `
 }
-
 //Messages Between logged in and profiled user in descending chronological order
+
